76f2594f190961ef4811d863a7004775e1c164ed
Jacobo Pita <jpitabaamondev23@fpcoruna.afundacion.org>
2024-10-15 13:54:48 +0200

    Mejorar placements de las labels, añadir comentarios

--- ej4CheckButton.py
+++ ej4CheckButton.py
@@ -3,7 +3,8 @@ from cProfile import label
 
 root = tk.Tk()
 root.title("Ejercicio 3")
-root.geometry("300x150")
+root.geometry("300x200")
+
 
 def mostrar_estado():
     estadoL = "Seleccionado" if var_leer.get() else "No seleccionado"
@@ -13,24 +14,28 @@ def mostrar_estado():
     estadoM = "Seleccionado" if var_musica.get() else "No seleccionado"
     labelM.config(text="Música: " + estadoM)
 
+
+# CheckLeer
 var_leer = tk.IntVar()
 leer = tk.Checkbutton(root, text="Leer", variable=var_leer, command=mostrar_estado)
-leer.pack()
-
+leer.place(x=0, y=0)
+# CheckDeporte
 var_deporte = tk.IntVar()
 deporte = tk.Checkbutton(root, text="Deporte", variable=var_deporte, command=mostrar_estado)
-deporte.pack()
-
+deporte.place(x=0, y=20)
+# CheckMusica
 var_musica = tk.IntVar()
 musica = tk.Checkbutton(root, text="Música", variable=var_musica, command=mostrar_estado)
-musica.pack()
+musica.place(x=0, y=40)
 
+tk.Label().pack()
+
+# Etiquetas
 labelL = tk.Label(root, text="Leer: No seleccionado")
-labelL.pack()
+labelL.place(x=100, y=2)
 labelD = tk.Label(root, text="Deporte: No seleccionado")
-labelD.pack()
+labelD.place(x=100, y=22)
 labelM = tk.Label(root, text="Música: No seleccionado")
-labelM.pack()
-
+labelM.place(x=100, y=42)
 
 root.mainloop()
